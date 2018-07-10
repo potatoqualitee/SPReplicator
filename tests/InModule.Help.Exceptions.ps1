09c217fa1c5a0e0fcb791e34fd9d60ff354459e6
Chrissy LeMaire <clemaire@gmail.com>
2018-07-09 23:15:07 -1000

    added Pester tests

---
+++ tests/InModule.Help.Exceptions.ps1
@@ -0,0 +1,5 @@
+﻿$global:FunctionHelpTestExceptions = @()
+
+$global:HelpTestEnumeratedArrays = @()
+
+$global:HelpTestSkipParameterType = @{}
---
+++ tests/constants.ps1
@@ -0,0 +1,4 @@
+$script:ModuleRoot = $PSScriptRoot
+$script:uri = "sharepoint2016"
+$script:mylist = "My List"
+$script:filename = "C:\temp\$script:mylist.xml"
