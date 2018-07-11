00b56c15a3f911530b9da2c105071a41a8a8b7e9
Chrissy LeMaire <clemaire@gmail.com>
2018-07-11 03:57:11 -1000

    updated description

--- functions/Get-SPRConnectedSite.ps1
+++ functions/Get-SPRConnectedSite.ps1
@@ -3,6 +3,9 @@
 .SYNOPSIS
     Returns the connected SharePoint Site Collection.
 
+.DESCRIPTION
+    Returns the connected SharePoint Site Collection.
+    
 .PARAMETER EnableException
     By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
     This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
