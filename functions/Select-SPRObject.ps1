9a968e99ef2e0dbdd9426a5d4875001fdb6c9a1c
Chrissy LeMaire <clemaire@gmail.com>
2018-07-20 06:23:13 -1000

    fixes

--- functions/Select-SPRObject.ps1
+++ functions/Select-SPRObject.ps1
@@ -36,14 +36,23 @@ EXAMPLE
         [string[]]$ExcludeProperty,
         [string]$TypeName
     )
-    begin {
-        if ($property) {            
-            # property needs to be string
-            if ("$property" -notlike "* as *") {
-                
+    process {
+        if ($TypeName) {
+            $InputObject.PSObject.TypeNames.Insert(0, "spreplicator.$TypeName")
+        }
+        
+        if ($ExcludeProperty) {
+            if ($InputObject.GetType().Name.ToString() -eq 'DataRow') {
+                $ExcludeProperty += 'Item', 'RowError', 'RowState', 'Table', 'ItemArray', 'HasErrors'
             }
-            else {
-            $newproperty = @()
+            
+            $props = ($InputObject | Get-Member | Where-Object MemberType -in 'Property','NoteProperty','AliasProperty' | Where-Object { $_.Name -notin $ExcludeProperty }).Name
+            $defaultset = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$props)
+        }
+        else {
+            # property needs to be string
+            if ("$property" -like "* as *") {
+                $newproperty = @()
                 foreach ($p in $property) {
                     if ($p -like "* as *") {
                         $old, $new = $p -isplit " as "
@@ -57,29 +66,14 @@ EXAMPLE
                 }
                 $property = $newproperty
             }
-        }
-    }
-    process {
-        if ($TypeName) {
-            $InputObject.PSObject.TypeNames.Insert(0, "spreplicator.$TypeName")
-        }
-
-        if ($ExcludeProperty) {
-            if ($InputObject.GetType().Name.ToString() -eq 'DataRow') {
-                $ExcludeProperty += 'Item', 'RowError', 'RowState', 'Table', 'ItemArray', 'HasErrors'
-            }
-
-            $properties = ($InputObject.PsObject.Members | Where-Object MemberType -ne 'Method' | Where-Object { $_.Name -notin $ExcludeProperty }).Name
-            $defaultset = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$properties)
-        }
-        else {
             $defaultset = New-Object System.Management.Automation.PSPropertySet('DefaultDisplayPropertySet', [string[]]$Property)
         }
-
+        
         $standardmembers = [System.Management.Automation.PSMemberInfo[]]@($defaultset)
-
+        
         # Do not be tempted to not pipe here
         $inputobject | Add-Member -Force -MemberType MemberSet -Name PSStandardMembers -Value $standardmembers -ErrorAction SilentlyContinue
+        
         $inputobject
     }
 }
