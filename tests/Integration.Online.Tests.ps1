d6f8fd90616d9319a28d8777eaef6e1c29ae800e
Chrissy LeMaire <clemaire@gmail.com>
2018-07-29 12:46:02 -1000

    fixed test to reflect proper excluded list

--- tests/Integration.Online.Tests.ps1
+++ tests/Integration.Online.Tests.ps1
@@ -14,7 +14,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
         $thislist = Get-SPRList -Site $script:onlinesite -Credential $script:onlinecred -List $script:mylist -WarningAction SilentlyContinue 3> $null
         $thislist = $thislist | Where-Object Title -ne 'SPRLog'
         $null = $thislist | Remove-SPRList -Confirm:$false -WarningAction SilentlyContinue 3> $null
-        $originallists = Get-SPRList | Where-Object Title -ne "SPReplicator"
+        $originallists = Get-SPRList | Where-Object Title -ne "SPRLog"
         $originalwebs = Get-SPRWeb
         if ($env:appveyor) {
             $loglist = Get-SPRList -List SPRLog
@@ -362,7 +362,7 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
 Describe "$CommandName Final Tests" -Tag "Finaltests" {
     Context "Checking to ensure all original data has remained" {
         $null = Connect-SPRSite -Site $script:onlinesite -Credential $script:onlinecred -Location Online
-        $nowlists = Get-SPRList | Where-Object Title -ne "SPReplicator"
+        $nowlists = Get-SPRList | Where-Object Title -ne "SPRLog"
         $nowwebs = Get-SPRWeb
         
         $originalsum = $originallists.ItemCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
