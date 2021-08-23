e6ad401d5b877c649cdfd706b007636b19ffe201
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-08-23 12:25:16 +0200

    MFA

--- SPReplicator/bin/library-import.ps1
+++ SPReplicator/bin/library-import.ps1
@@ -4,9 +4,11 @@ if ($PSVersionTable.PSEdition -eq "Core") {
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.Runtime.Portable.dll"
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.Portable.dll"
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.UserProfiles.Portable.dll"
-}
-else {
+    Add-Type -Path "$script:ModuleRoot\bin\OfficeDevPnP.Core.dll"
+} else {
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.dll"
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.Runtime.dll"
     Add-Type -Path "$script:ModuleRoot\bin\Microsoft.SharePoint.Client.UserProfiles.dll"
+    Add-Type -Path "$script:ModuleRoot\bin\OfficeDevPnP.Core.dll"
+
 }
