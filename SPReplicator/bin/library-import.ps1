52045606e81e9209b2506911a14e7bfaa859e8d9
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-08-24 12:55:53 +0200

    auth and ps

--- SPReplicator/bin/library-import.ps1
+++ SPReplicator/bin/library-import.ps1
@@ -4,7 +4,11 @@
 if ($PSVersionTable.PSEdition -eq "Core") {
     Add-Type -Path "$script:ModuleRoot\bin\PnP.Framework.dll"
     Add-Type -Path "$script:ModuleRoot\bin\PnP.Core.dll"
+    Add-Type -Path "$script:ModuleRoot\bin\PnP.Core.Auth.dll"
+    Add-Type -Path "$script:ModuleRoot\bin\PnP.PowerShell.dll"
 } else {
     Import-Module "$script:ModuleRoot\bin\PnP.Framework.dll"
     Import-Module "$script:ModuleRoot\bin\PnP.Core.dll"
+    Import-Module "$script:ModuleRoot\bin\PnP.Core.Auth.dll"
+    Add-Type -Path "$script:ModuleRoot\bin\PnP.PowerShell.dll"
 }
