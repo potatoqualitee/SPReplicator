d3fd1f3fe7700031d30e8873272baa74f1b18845
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-08-24 13:21:40 +0200

    this one

--- SPReplicator/bin/library-import.ps1
+++ SPReplicator/bin/library-import.ps1
@@ -1,14 +1,2 @@
-﻿Get-ChildItem "$script:ModuleRoot\bin\" -Recurse | Unblock-File -ErrorAction SilentlyContinue
-
-
-if ($PSVersionTable.PSEdition -eq "Core") {
-    Add-Type -Path "$script:ModuleRoot\bin\PnP.Framework.dll"
-    Add-Type -Path "$script:ModuleRoot\bin\PnP.Core.dll"
-    Add-Type -Path "$script:ModuleRoot\bin\PnP.Core.Auth.dll"
-    Add-Type -Path "$script:ModuleRoot\bin\PnP.PowerShell.dll"
-} else {
-    Import-Module "$script:ModuleRoot\bin\PnP.Framework.dll"
-    Import-Module "$script:ModuleRoot\bin\PnP.Core.dll"
-    Import-Module "$script:ModuleRoot\bin\PnP.Core.Auth.dll"
-    Add-Type -Path "$script:ModuleRoot\bin\PnP.PowerShell.dll"
-}
+﻿Add-Type -Path "$script:ModuleRoot\bin\OfficeDevPnP.Core.dll"
+#Import-Module "$script:ModuleRoot\bin\PnP.PowerShell" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
